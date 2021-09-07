23d666fad232cd023bf18e9ee47cd1a72bb8fa8d
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2021-09-07 13:30:52 -0400

    Move ASFF to Sub-Control level, put results set in id, put code_desc SHA256 hash in id

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 import { ASFFFinding } from './types/asff';
 import { HDF } from './types/hdf';
 import * as _ from 'lodash';
-import {v4} from 'uuid';
+import {createHash} from 'crypto'
 
 const program = new Command();
 program.version('1.0.0')
@@ -44,81 +44,89 @@ function sliceIntoChunks(arr: ASFFFinding[], chunkSize: number) {
     return res;
 }
 
+// Gets rid of extra spacing + newlines as these aren't shown in Security Hub
+function cleanText(text?: string): string | undefined {
+    if (text){
+        return text.replace(/  +/g, ' ').replace(/\r?\n|\r/g, '')
+    } else {
+        return undefined
+    }
+    
+}
+
 hdf.profiles.forEach((profile) => {
     profile.controls.forEach((control) => {
-        const controlStatus = control.results.every((result) => (result.status === 'passed' || result.status === 'skipped'))
-        const segments = _.truncate(control.results.map((control) => control.status + ': ' + control.code_desc + (control.message ? ': ' + control.message : '')).join(' -------------------- '), {length: 512})
-        const asffControl: ASFFFinding = {
-            SchemaVersion: "2018-10-08",
-            Id: `${profile.name}/${control.id}/finding/${v4()}`,
-            ProductArn: `arn:aws:securityhub:us-east-2:${options.awsAccountId}:product/${options.awsAccountId}/default`,
-            AwsAccountId: options.awsAccountId,
-            Types: ["Software and Configuration Checks"],
-            CreatedAt: (control.results[0] || {start_time: new Date().toISOString()}).start_time,
-            Region: options.region,
-            UpdatedAt: new Date().toISOString(),
-            GeneratorId: `arn:aws:securityhub:us-east-2:${options.awsAccountId}:ruleset/set/${profile.name}/v1.0.0/rule/${control.id}`,
-            Title: _.truncate(`${profile.name}/${control.id} ${control.title}`, {length: 256}),
-            Description: _.truncate(control.desc, {length: 1024}),
-            FindingProviderFields: {
-                Severity: {
-                    Label: impactMapping.get(control.impact) || 'INFORMATIONAL',
-                    Original: impactMapping.get(control.impact) || 'INFORMATIONAL'
+        control.results.forEach((segment) => {
+            const controlStatus = segment.status == 'passed'
+            const asffControl: ASFFFinding = {
+                SchemaVersion: "2018-10-08",
+                Id: `${profile.name}/${control.id}/finding/${options.input}/${createHash('sha256').update(segment.code_desc).digest('hex')}`,
+                ProductArn: `arn:aws:securityhub:us-east-2:${options.awsAccountId}:product/${options.awsAccountId}/default`,
+                AwsAccountId: options.awsAccountId,
+                Types: ["Software and Configuration Checks"],
+                CreatedAt: (control.results[0] || {start_time: new Date().toISOString()}).start_time,
+                Region: options.region,
+                UpdatedAt: new Date().toISOString(),
+                GeneratorId: `arn:aws:securityhub:us-east-2:${options.awsAccountId}:ruleset/set/${profile.name}/v1.0.0/rule/${control.id}`,
+                Title: _.truncate(`${profile.name}/${control.id} ${cleanText(control.title)}`, {length: 256}),
+                Description: _.truncate(cleanText(control.desc), {length: 1024}),
+                FindingProviderFields: {
+                    Severity: {
+                        Label: impactMapping.get(control.impact) || 'INFORMATIONAL',
+                        Original: impactMapping.get(control.impact) || 'INFORMATIONAL'
+                    },
+                    Types: [`Profile/Name/${profile.name}`, `Profile/Version/${profile.version}`, `Profile/SHA256/${profile.sha256}`, `Profile/Title/${profile.title}`, `Profile/Maintainer/${profile.maintainer}`, `Profile/Summary/${profile.summary}`, `Profile/License/${profile.license}`, `Profile/Copyright/${profile.copyright}`,  `Profile/Copyright Email/${profile.copyright_email}`]
                 },
-                Types: [`Profile/Name/${profile.name}`, `Profile/Version/${profile.version}`, `Profile/SHA256/${profile.sha256}`, `Profile/Title/${profile.title}`, `Profile/Maintainer/${profile.maintainer}`, `Profile/Summary/${profile.summary}`, `Profile/License/${profile.license}`, `Profile/Copyright/${profile.copyright}`,  `Profile/Copyright Email/${profile.copyright_email}`]
-            },
-            Remediation: {
-                Recommendation: {
-                    Text: _.truncate((control.descriptions?.find((description) => description.label === 'fix') || {data: control.fix || 'Fix not available'}).data, {length: 512})
-                }
-            },
-            ProductFields: {
-                "Provider Name": "AWS Systems Manager Compliance"
-            },
-            Severity: {
-                Product: 1,
-                Normalized: 10,
-            },
-            Resources: [
-                {
-                    Type: "AwsAccount",
-                    Id: `AWS::::Account:${options.awsAccountId}`,
-                    Partition: "aws",
-                    Region: options.region
-                }
-            ],
-            Compliance: {
-                Status: controlStatus ? 'PASSED' : 'FAILED',
-                StatusReasons: [
-                    {
-                        ReasonCode: controlStatus ? 'CONFIG_EVALUATIONS_EMPTY' : 'CLOUDTRAIL_METRIC_FILTER_NOT_VALID',
-                        Description: _.truncate(control.results.map((result) => result.code_desc + "\n\n" + result.message).join('\n\n'), {length: 2048})
+                Remediation: {
+                    Recommendation: {
+                        Text: _.truncate(cleanText((control.descriptions?.find((description) => description.label === 'fix') || {data: control.fix || 'Fix not available'}).data), {length: 512})
                     }
-                ]
-            }
-        }
-        if(segments.length) {
-            asffControl.Note = {
-                Text: segments,
-                UpdatedBy: "Segments",
-                UpdatedAt: new Date().toISOString()
-            }
-        }
-        for (const tag in control.tags) {
-            if(control.tags[tag]) {
-                if(tag === 'nist' && Array.isArray(control.tags.nist)) {
-                    asffControl.FindingProviderFields?.Types.push(`NIST/800-53/${control.tags.nist.join(', ')}`)
-                } else if (tag === 'cci' && Array.isArray(control.tags.cci)) {
-                    asffControl.FindingProviderFields?.Types.push(`DISA/CCI/${control.tags.cci.join(', ')}`)
-                } else if (typeof control.tags[tag] === 'string') {
-                    asffControl.FindingProviderFields?.Types.push(`Other/${tag.replace(/\W/g, '')}/${(control.tags[tag] as string).replace(/\W/g, '')}`)
-                } else if (typeof control.tags[tag] === 'object' && Array.isArray(control.tags[tag])) {
-                    asffControl.FindingProviderFields?.Types.push(`Other/${tag.replace(/\W/g, '')}/${(control.tags[tag] as Array<string>).join(', ').replace(/\W/g, '')}`)
+                },
+                ProductFields: {
+                    "Provider Name": "AWS Systems Manager Compliance"
+                },
+                Note: {
+                    Text: _.truncate(cleanText(segment.code_desc), {length: 512}),
+                    UpdatedAt: new Date().toISOString(),
+                    UpdatedBy: 'Description',
+                },
+                Severity: {
+                    Product: 1,
+                    Normalized: 10,
+                },
+                Resources: [
+                    {
+                        Type: "AwsAccount",
+                        Id: `AWS::::Account:${options.awsAccountId}`,
+                        Partition: "aws",
+                        Region: options.region
+                    }
+                ],
+                Compliance: {
+                    Status: controlStatus ? 'PASSED' : 'FAILED',
+                    StatusReasons: [
+                        {
+                            ReasonCode: controlStatus ? 'CONFIG_EVALUATIONS_EMPTY' : 'CLOUDTRAIL_METRIC_FILTER_NOT_VALID',
+                            Description:  _.truncate(cleanText(segment.message) || 'Unavailable', {length: 2048})
+                        }
+                    ]
                 }
             }
-        }
-        
-        findings.push(asffControl)
+            for (const tag in control.tags) {
+                if(control.tags[tag]) {
+                    if(tag === 'nist' && Array.isArray(control.tags.nist)) {
+                        asffControl.FindingProviderFields?.Types.push(`NIST/800-53/${control.tags.nist.join(', ')}`)
+                    } else if (tag === 'cci' && Array.isArray(control.tags.cci)) {
+                        asffControl.FindingProviderFields?.Types.push(`DISA/CCI/${control.tags.cci.join(', ')}`)
+                    } else if (typeof control.tags[tag] === 'string') {
+                        asffControl.FindingProviderFields?.Types.push(`Other/${tag.replace(/\W/g, '')}/${(control.tags[tag] as string).replace(/\W/g, '')}`)
+                    } else if (typeof control.tags[tag] === 'object' && Array.isArray(control.tags[tag])) {
+                        asffControl.FindingProviderFields?.Types.push(`Other/${tag.replace(/\W/g, '')}/${(control.tags[tag] as Array<string>).join(', ').replace(/\W/g, '')}`)
+                    }
+                }
+            }
+            findings.push(asffControl)
+        })
     })
 })
 
